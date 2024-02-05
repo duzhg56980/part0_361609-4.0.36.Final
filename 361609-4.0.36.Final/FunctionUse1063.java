6c4d64448a58175781c5a3939053d2a5d767afb6
duzhg56980 <guokey1996@gmail.com>
2024-02-05 16:42:28 +0800

    init commit

---
+++ 361609-4.0.36.Final/FunctionUse1063.java
@@ -0,0 +1,6 @@
+import io.netty.handler.stream.ChunkedStream;
+public class FunctionUse1063 {
+public void funcUse() {
+ChunkedStream chunkedstream = new ChunkedStream();
+}
+}
---
+++ 361609-4.0.36.Final/FunctionUse917.java
@@ -0,0 +1,6 @@
+import io.netty.handler.traffic.GlobalTrafficShapingHandler.PerChannel;
+public class FunctionUse917 {
+public void funcUse() {
+PerChannel perchannel = new PerChannel();
+}
+}
